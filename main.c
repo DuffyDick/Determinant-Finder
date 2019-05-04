db989bc2f988d6754192ca2301ee7ecbb3d3b8a9
amnesia <antonio.renzullo1568@gmail.com>
2019-05-04 14:25:59 +0200

    primi progressi reali

--- main.c
+++ main.c
@@ -30,14 +30,8 @@ int take_matrix(int A[50][50],int i){
 
 
 int check(int A[50][50],int i){
-    int det = 0;
-    if (i == 2){
-        det = detereminant_2x2(A);
-    }else if (i == 1){
-        det = A[0][0];
-    }
-
-    return det;
+    if (i == 1 ||  i == 2)
+        return 1;
 }
 
 int dim_count(int A[50][50],int i){
@@ -49,7 +43,6 @@ int dim_count(int A[50][50],int i){
     return count;
 }
 
-
 int detereminant_2x2(int A[50][50]){
     return (A[0][0]*A[1][1]) - (A[0][1]*A[1][0]);
 }
@@ -57,7 +50,7 @@ int detereminant_2x2(int A[50][50]){
 
 int find_submatrix(int A[50][50],int B[50][50],int max ,int line, int rem){
     int i = 0, j = 0;
-    for (i = 0; i < rem; i++){
+    for (i = 0; i < line; i++){
         for (j = 0; j < rem; j++){
             B[i][j] = A[i][j];
         }
@@ -66,7 +59,7 @@ int find_submatrix(int A[50][50],int B[50][50],int max ,int line, int rem){
         }
     }
 
-    for (i = rem+1; i < max; i++){
+    for (i = line+1; i < max; i++){
          for (j = 0; j < rem; j++){
             B[i-1][j] = A[i][j];
         }
@@ -74,6 +67,18 @@ int find_submatrix(int A[50][50],int B[50][50],int max ,int line, int rem){
             B[i-1][j-1] = A[i][j];
         }
     }
+}
+
+
+int banal(int A[50][50],int dim){
+    int det = 0;
+    if (dim == 2)
+        det = detereminant_2x2(A);
+    else
+        det = A[0][0];
+
+    return det;
+    
 }
 
 
@@ -87,36 +92,35 @@ int algebric_complement(int A[50][50],int min,int i, int j){
 int main(){
     int B[50][50];
     int A[50][50];
-    int i = 0, dim = 0, det = 0;
+    int i = 0, j = 0;
+    int det = 0;
+    int dim = 0;
     printf("Inserire dimensione matrice: ");
     scanf("%d",&i);
     take_matrix(A,i);
     printf("\e[1;1H\e[2J");
     show_matrix(A,i);
-    det = check(A,i);
-    
 
-
-
-    find_submatrix(A,B,i,1);
-    dim = dim_count(B,i-1);
-    
-    if (dim == 2){
-        det = detereminant_2x2(B,dim);
-    }else{
-        while(dim > 2){
-            find_submatrix(B,B,dim,1);
-
-        }
-
+    if (check(A,i)){
+        det = banal(A,i);
+        printf("Il determinante e': %d\n\n", det);
+        return 0;
     }
 
-    for (i = 1; ;){
+
+    
+    int array[50];
+    int k = 0, h = 0;
+    for (int h = 1; ;){
         for (j = 0; j < i; j++){
-            find_submatrix(A,B,dim,1,j);
-
+            find_submatrix(A,B,i,1,j);
+            dim = check(B,i-1);
+            if (dim == 1 || dim == 2){
+                array[k] = banal(B,dim);
+                k++;
+            }  
         }
     }
-        
+
     return 0;
 }
